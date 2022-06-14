a63fbdcc34b5af8ed0f37b0574e72edde9a1f245
cllei12 <lei_cao2@outlook.com>
2022-06-13 21:18:06 -0700

    ReactFacts static page

--- learning-react/static_page_toolchains/components/Navbar.js
+++ learning-react/static_page_toolchains/components/Navbar.js
@@ -5,9 +5,9 @@ const reactLofoURL = "../images/react-icon-small.png"
 function Navbar(){
     return (
         <nav>
-            <img src={reactLofoURL} alt="reactLofo" />
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img className="nav-img" src={reactLofoURL} alt="reactLofo" />
+            <h3 className="nav-logo-text">ReactFacts</h3>
+            <h4 className="nav-title">React Course - Project 1</h4>
         </nav>
     )
 }
