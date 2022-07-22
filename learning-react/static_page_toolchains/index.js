61a891bea1f8c117a9b2d889dd53315dcbf75e13
cllei12 <lei_cao2@outlook.com>
2022-07-21 17:12:34 -0700

    Restructure

--- learning-react/static_page_toolchains/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./style.css"
-import App from "./App"
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
-// or
-// ReactDOM.render(<App />, document.getElementById("root"))
